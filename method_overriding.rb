395330387c02f7c36417b0a3e42a06a36f42f051
vicky099 <vikky.panmand@gmail.com>
2019-03-18 23:02:11 +0530

    using super method we can access variables defined in subclass to super class

--- method_overriding.rb
+++ method_overriding.rb
@@ -7,10 +7,29 @@ end
 
 class Test4 < Test3
 	def a1
-		#super -> with the help of this we can call super class method
 		puts "<<<<< Test4 method a1"
 	end
 end
 
 b = Test4.new
 b.a1
+
+
+
+#using super method - by using super call to parent class method, we can access all instance variables defined in subclass to super class.
+class Test5
+	def a1
+		p "<<<< called from super : #{@a}"
+	end
+end
+
+class Test6 < Test5
+	def a1
+		@a = "Test6 instance variable"
+		super
+		p "<<<< Test 6 class"
+	end
+end
+
+s = Test6.new
+s.a1
